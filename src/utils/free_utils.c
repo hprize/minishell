a84a14a3d25894d086df13fe983ef0ae6ffca9c6
Hyebin Lee <hyebinle@c2r2s3.42gyeongsan.kr>
2024-12-01 17:45:19 +0900

    폴더 정리

---
+++ src/utils/free_utils.c
@@ -0,0 +1,44 @@
+#include "../minishell.h"
+
+
+// ----- free 함수 -----
+
+void	free_tree(t_tree *node)
+{
+	int	i;
+
+	if (node == NULL)
+		return;
+	i = 0;
+	while (i < node->child_count)
+	{
+		free_tree(node->children[i]);
+		i++;
+	}
+	free(node->children);
+	node->children = NULL;
+	free(node->value);
+	node->value = NULL;
+	free(node);
+}
+
+void	free_tokens(t_token *tokens) {
+	t_token	*next;
+
+	while (tokens != NULL)
+	{
+		next = tokens->next;
+		if (tokens->value)
+			free(tokens->value);
+		free(tokens);
+		tokens = next;
+	}
+}
+
+void	free_master(t_envp *master)
+{
+	ft_arrfree(master->path_list);
+	ft_arrfree(master->envp);
+	free_node(master->u_envp);
+	free(master);
+}
