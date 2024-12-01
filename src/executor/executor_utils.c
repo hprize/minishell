a502d136bd5368776ff691462179be9e5323b94a
Junhu Lee <junlee@c2r2s1.42gyeongsan.kr>
2024-12-01 18:06:20 +0900

    executor, parser 폴더 정리

---
+++ src/executor/executor_utils.c
@@ -0,0 +1,57 @@
+#include "../minishell.h"
+
+void	restore_stdio(int saved_stdin, int saved_stdout)
+{
+	if (dup2(saved_stdin, STDIN_FILENO) < 0)
+		perror("Failed to restore stdin");
+	close(saved_stdin);
+
+	if (dup2(saved_stdout, STDOUT_FILENO) < 0)
+		perror("Failed to restore stdout");
+	close(saved_stdout);
+}
+
+t_tree	*find_cmd_node(t_tree *node)
+{
+	int		i;
+	t_tree	*now;
+
+	now = node;
+	i = 0;
+	while (i < now->child_count)
+	{
+		if (now->children[i]->type == NODE_CMD)
+			return (now->children[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+char	**each_args(t_tree *node, t_envp *master, int cnt)
+{
+	char	**args;
+	int		i;
+	int		j;
+
+	args = malloc(sizeof(char *) * (node->child_count + cnt + 1));
+	if (args == NULL)
+		exit(1);
+	if (cnt == 1)
+	{
+		args[0] = return_absolute_path(node, master);
+		if (args[0] == NULL)
+			exit(EXIT_FAILURE);
+	}
+	i = cnt;
+	j = 0;
+	while (j < node->child_count)
+	{
+		args[i] = ft_strdup(node->children[j]->value);
+		if (args[i] == NULL)
+			exit(EXIT_FAILURE);
+		i++;
+		j++;
+	}
+	args[i] = NULL;
+	return (args);
+}
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,7 +1,34 @@
 #ifndef PARSER_H
 # define PARSER_H
-#include "parser_struct.h"
-#include "../../minishell.h"
+
+# include "parser_struct.h"
+# include "../../minishell.h"
+
+// -- parser.c -- //
+t_tree	*parse_pipe(t_token **current);
+t_tree	*parse_exec(t_token **current);
+void	parse_reds_opt(t_tree *exec_node, t_token **current);
+t_tree	*parse_reds(t_token **current);
+t_tree	*parse_cmd(t_token **current);
+t_tree	*parse(t_token *tokens);
+
+// -- parser_utils.c -- //
+void	add_token(t_token **head, t_token **current, t_token *new_token);
+t_token	*create_token(token_type type, const char *value);
+void	merge_token(t_token **current, char *new_value);
+t_tree	*create_tree_node(node_type type, const char *value);
+void	add_child(t_tree *parent, t_tree *child);
+
+// -- tokenize.c -- //
+void	process_op(t_token **head, t_token **current, t_token_context *ctx);
+void	process_quote(t_token **head, t_token **current, t_env *u_envp, t_token_context *ctx);
+void	process_word(t_token **head, t_token **current, t_env *u_envp, t_token_context *ctx);
+t_token	*tokenize(const char *input, t_env *u_envp);
+
+// -- replace_env -- //
+char	*ft_strjoin_free2(char *s1, char *s2);
+int	is_valid_env_char(char c, int is_first);
+void	process_env_replacement(char **value, t_env *u_envp);
 
 
 #endif
